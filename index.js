c031fb31f6379b0222ae1fb85c8c24ff10d27eab
Maxwell <max@dirty-martini.com>
2022-03-07 22:32:45 +0000

    second

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ import figlet from "figlet"
 import boxen from "boxen"
 import { createSpinner } from "nanospinner"
 
-import path from "path"
 import os from "os"
 import process from "process"
+import nodegit from "nodegit"
+import path from "path"
+import { URL } from "url"
+// import fs from 'fs-extra'
 
 let themeName
 let proxyName
@@ -39,11 +42,11 @@ async function handleAnswer() {
 async function welcome() {
   figlet.text("DMTHEME", { font: "Standard" }, function (err, data) {
     console.log(data)
-  })
-  figlet.text("INSTALLER", { font: "Standard" }, function (err, data) {
-    console.log(data)
-    console.log("Let's get this party started!")
-    console.log("Press any key to continue...")
+    figlet.text("INSTALLER", { font: "Standard" }, function (err, data) {
+      console.log(data)
+      console.log("Let's get this party started!")
+      console.log("Press any key to continue...")
+    })
   })
 
   await keypress()
@@ -136,14 +139,47 @@ async function readyDownload() {
           // Change the directory
           process.chdir(directory)
           console.log("New directory: " + process.cwd())
+
+          // download git repo
+          const url = "git@github.com:dirtymartiniexe/DMThemev4.git"
+          const cloneOpts = {
+            fetchOpts: {
+              callbacks: {
+                certificateCheck: () => 1,
+                credentials: (url, username) => {
+                  if (authAttempted) return nodegit.Cred.defaultNew()
+                  authAttempted = true
+                  if (url.startsWith("https://") && url.includes("@")) {
+                    url = new URL(url)
+                    return nodegit.Cred.userpassPlaintextNew(
+                      url.username,
+                      url.password
+                    )
+                  } else {
+                    return nodegit.Cred.sshKeyFromAgent(username)
+                  }
+                },
+              },
+            },
+          }
+
+          const newDirectory = directory + "/" + themeName
+          let authAttempted = false
+          nodegit
+            .Clone(url, newDirectory, cloneOpts)
+            .then(function (repo) {
+              // change folder name to variable
+              console.log(
+                "Cloned " + path.basename(url) + " to " + repo.workdir()
+              )
+            })
+            .catch(function (err) {
+              console.log(err)
+            })
         } catch (err) {
           // Printing error if occurs
           console.error("Error while changing directory:" + err)
         }
-
-        // download git repo
-
-        // change folder name to variable
 
         // go to webpack.config.js ammend proxy to variable
 
